a10fa55e5597724112bae06be29868192ff58819
Neslihan Çakırbaş <30401423+1neslihan@users.noreply.github.com>
2022-07-14 21:51:46 +0300

    Algoritma Sorulari-2

--- Program.cs
+++ Program.cs
@@ -1,37 +1,51 @@
 ﻿using System;
-namespace cs_calisma
+namespace alistirma
 {
     class Program
     {
         static void Main(string[] args)
         {
-           int sayiAdet;
+
+            /*
+            Kullanicidan iki adet sayi alinir n sayisi ve m sayisi
+            daha sonra n sayisi kadar sayi girmesi istenir ve m sayisiyla eşit olanları 
+            veya m'e tam bölünenleri ekrana yazdırır.
+            */
+           Console.WriteLine("iki pozitif tam sayi giriniz: ");
+           int n;
+           int m;
            
-           Console.Write("Kac adet sayi girmek istiyorsunuz?: ");
 
-           sayiAdet=Convert.ToInt32(Console.ReadLine());
+           Console.Write("1. sayi n(sayisi): ");
+           n=Convert.ToInt32(Console.ReadLine());
 
-           int[] dizi=new int[sayiAdet];
+           int[] ndizi=new int[n];
+           Console.WriteLine();
+           Console.Write("2. sayi m(sayisi): ");
+           m=Convert.ToInt32(Console.ReadLine());
 
-           for(int i=0;i<sayiAdet;i++){
+           Console.WriteLine();
+           Console.WriteLine(n +" adet sayi giriniz.");
+           Console.WriteLine();
 
-            Console.Write( (i+1) +". sayiyi giriniz: ");
-            dizi[i]=Convert.ToInt32(Console.ReadLine());
+           for(int i=0;i<n;i++){
+
+            Console.Write((i+1) +". sayi: ");
+            ndizi[i]=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
 
            }
+           Console.WriteLine();
+           Console.WriteLine("girilen sayilardan m'e eşit olan veya tam bölünenler");
 
-          Console.WriteLine("Çift elemanlar: ");
 
-           for(int i=0; i<dizi.Length;i++){
-
-            if(dizi[i]%2==0){
-
-                Console.WriteLine(dizi[i]);
-                
+           for(int i=0; i<n;i++){
+            if(ndizi[i]== m || ndizi[i]%m==0){
+                Console.WriteLine(ndizi[i]);
             }
-
            }
-        
+
+           
            
            
 

5a5e55da034ea373546a6f4a60deb2f20fb5fbca
Neslihan Çakırbaş <30401423+1neslihan@users.noreply.github.com>
2022-07-14 22:31:39 +0300

    Algoritma Soruları-4

---
+++ Program3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace alistirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Kullanıcıdan alınan bir cümledek, kelime ve harf sayısını ekrana yazdırma*/
+
+           string str;
+
+           Console.Write("Bir cumle giriniz: ");
+           str=Console.ReadLine();
+
+           string[] sdizi =str.Split(' ');
+
+           int counter=0;
+
+           foreach(var items in sdizi){
+            counter++;
+           }
+
+           Console.WriteLine();
+           Console.Write("Cumledeki kelime sayisi: "+ counter);
+           Console.WriteLine();
+           str=str.Replace(" ","");
+           Console.Write("Cumledeki harf sayisi: "+ str.Length);
+
+
+
+
+           
+           
+
+
+        }
+    }
+
+}

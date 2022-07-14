e58c661280cae5ad9311be610104ba144e43f125
Neslihan Çakırbaş <30401423+1neslihan@users.noreply.github.com>
2022-07-14 22:08:39 +0300

    Algoritma Soruları-3

---
+++ Program2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace alistirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Kullanicidan iki adet sayi alinir n sayisi ve m sayisi
+            daha sonra n sayisi kadar sayi girmesi istenir ve m sayisiyla eşit olanları 
+            veya m'e tam bölünenleri ekrana yazdırır.
+            */
+           Console.WriteLine("iki pozitif tam sayi giriniz: ");
+           int n;
+           int m;
+           
+
+           Console.Write("1. sayi n(sayisi): ");
+           n=Convert.ToInt32(Console.ReadLine());
+
+           int[] ndizi=new int[n];
+           Console.WriteLine();
+           Console.Write("2. sayi m(sayisi): ");
+           m=Convert.ToInt32(Console.ReadLine());
+
+           Console.WriteLine();
+           Console.WriteLine(n +" adet sayi giriniz.");
+           Console.WriteLine();
+
+           for(int i=0;i<n;i++){
+
+            Console.Write((i+1) +". sayi: ");
+            ndizi[i]=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+           }
+           Console.WriteLine();
+           Console.WriteLine("girilen sayilardan m'e eşit olan veya tam bölünenler");
+
+
+           for(int i=0; i<n;i++){
+            if(ndizi[i]== m || ndizi[i]%m==0){
+                Console.WriteLine(ndizi[i]);
+            }
+           }
+
+           
+           
+           
+
+
+        }
+    }
+}
